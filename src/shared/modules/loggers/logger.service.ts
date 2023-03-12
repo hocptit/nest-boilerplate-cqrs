4b74e53e2a797f862082c0bedb5ba2b2972718cd
Patrick Nguyen <hoc.nguyen@sotatek.com>
2023-03-12 15:37:39 +0700

    feat: add datadog

--- src/shared/modules/loggers/logger.service.ts
+++ src/shared/modules/loggers/logger.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { Appender, configure, getLogger, Layout, Logger } from 'log4js';
+import {
+  Appender,
+  configure,
+  DateFileAppender,
+  getLogger,
+  Layout,
+  Logger,
+} from 'log4js';
 import { ConfigService } from '@nestjs/config';
 import { EEnvKey } from '@constants/env.constant';
+import { datadogLogs } from '@datadog/browser-logs';
 
 const layouts: Record<string, Layout> = {
   console: {
@@ -14,7 +22,7 @@ const layouts: Record<string, Layout> = {
   },
 };
 
-const appenders: Record<string, Appender> = {
+const appenders: Record<string, Appender | DateFileAppender> = {
   console: {
     type: 'console',
     layout: layouts.console,
@@ -22,8 +30,10 @@ const appenders: Record<string, Appender> = {
   dateFile: {
     type: 'dateFile',
     filename: 'logs/out.log',
+    maxLogSize: 10000000,
     pattern: '-yyyy-MM-dd',
     layout: layouts.dateFile,
+    keepFileExt: true,
   },
   dateFileAccess: {
     type: 'dateFile',
@@ -51,6 +61,12 @@ export class LoggerService {
   constructor(private configService: ConfigService) {
     const level = configService.get(EEnvKey.LOG_LEVEL);
     const isWriteLog = configService.get(EEnvKey.IS_WRITE_LOG) === 'true';
+    datadogLogs.init({
+      clientToken: configService.get(EEnvKey.DATADOG_LOGS_CLIENT_TOKEN),
+      site: configService.get(EEnvKey.DATADOG_LOGS_URL),
+      forwardErrorsToLogs: true,
+      sessionSampleRate: 100,
+    });
     configure({
       appenders: appenders,
       categories: {
@@ -74,6 +90,7 @@ export class LoggerPort {
   constructor(protected logger: Logger) {}
   info(...args: any[]) {
     this.logger.info(args);
+    datadogLogs.logger.info('Button clicked', { name: 'buttonName', id: 123 });
   }
   debug(...args: any[]) {
     this.logger.debug(args);
