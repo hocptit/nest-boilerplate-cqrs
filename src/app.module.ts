5842e454036a23dbc34f0679161d34f0faf0739e
Patrick Nguyen <hoc.nguyen@sotatek.com>
2023-03-19 13:34:34 +0700

    feat: add travis

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,6 @@ import { MODULES } from './modules';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 
-
 @Module({
   imports: [
     ConfigurationModule,
